0cabcbb4debe2587b1f9cbe436c75d3addd31110
kostya <mcantoharip@gmail.com>
2025-03-31 20:03:54 +0300

    комит

--- main.py
+++ main.py
@@ -215,6 +215,3 @@ def reqister():
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
 
-
-if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
